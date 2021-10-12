e0703831caddd275ac47f7a499aa3534d60ec9f9
Simek <gosimek@gmail.com>
2021-10-12 13:40:23 +0200

    [modules-core][speech] remove listeners count workaround

--- packages/expo-speech/src/Speech/Speech.ts
+++ packages/expo-speech/src/Speech/Speech.ts
@@ -126,12 +126,7 @@ export async function resume(): Promise<void> {
 }
 
 function setSpeakingListener(eventName, callback) {
-  // @ts-ignore: the EventEmitter interface has been changed in react-native@0.64.0
-  const listenerCount = SpeechEventEmitter.listenerCount
-    ? // @ts-ignore: this is available since 0.64
-      SpeechEventEmitter.listenerCount(eventName)
-    : // @ts-ignore: this is available in older versions
-      SpeechEventEmitter.listeners(eventName).length;
+  const listenerCount = SpeechEventEmitter.listenerCount(eventName);
   if (listenerCount > 0) {
     SpeechEventEmitter.removeAllListeners(eventName);
   }
